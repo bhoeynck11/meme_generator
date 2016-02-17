c1bde5887a909ceadc37e1550f6e83b2474aff03
Brian Hoeynck <bhoeynck11@gmail.com>
2016-02-17 10:32:20 -0600

    Initial commits to meme generator site, including folder structure and index.html file

---
+++ memes.js
@@ -0,0 +1,58 @@
+$(document).ready(function() {
+  
+    
+//Take a user-supplied URL and change the img's src attribute to that URL when the "change picture" button is pressed. To make better...find a way to allow uploadable files from user's computer?
+$("#pic-btn").click(function() {
+    var storeURL = $("#url-entry").val();
+    $('#meme-pic').attr("src", storeURL);  
+});
+    
+    
+//Change the text of the upper caption
+$("#top-caption-input").keyup(function() {   
+    var userInputTop=$(this).val();
+    $("#top-caption").text(userInputTop);
+});
+
+//Change the text of the lower caption
+$("#bottom-caption-input").keyup(function() {   
+    var userInputBottom=$(this).val();
+    $("#bottom-caption").text(userInputBottom);
+});
+
+/*jQuery API: the .text() method CANNOT be used to get the inner text of a form element. For that, we had to use the .val() method (as above). This tripped me up for a long time until I went to the .text() page on the documentation.*/
+    
+
+    
+//Ways to improve: If the text is white by default, can the user click a button that will change the text to red, or to black, or to other options?
+    
+    $("#red").click(function() {
+       $(".captions").css("color", "red"); 
+    });
+    
+    $("#green").click(function() {
+       $(".captions").css("color", "green"); 
+    });
+    
+    $("#black").click(function() {
+       $(".captions").css("color", "black"); 
+    });
+    $("#white").click(function() {
+       $(".captions").css("color", "white"); 
+    });
+    
+    $("#orange").click(function() {
+       $(".captions").css("color", "orange"); 
+    });
+    
+    $("#blue").click(function() {
+       $(".captions").css("color", "blue"); 
+    });
+    $("#yellow").click(function() {
+       $(".captions").css("color", "yellow"); 
+    });  
+    
+//Ok, now a way to make it DRY?
+
+    
+}); //end document ready
